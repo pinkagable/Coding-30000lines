f23438a6e5254090eca379aa863478b036736099
pinkagable <112944135+pinkagable@users.noreply.github.com>
2023-03-18 21:42:43 +0800

    Add files via upload

--- c-30000lines/230312.c
+++ c-30000lines/230312.c
@@ -142,3 +142,162 @@ int main()
 //	printf("%d", a > b ? a: b);
 //	return 0;
 //}
+
+//判断一个数是否能被五整除
+//#include<stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	if (m % 5 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 20;
+//	if (age >= 18)
+//		printf("chengnian\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 28;
+//	if (age <= 18)
+//		printf("未成年\n");
+//	else if (age <= 26)
+//		printf("青年\n");
+//	else  (age <= 40)
+//		printf("壮年\n");
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//		if (b == 2)
+//			printf("hehe\n");
+//		else
+//			printf("haha\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 3;
+//	if (num = 5)
+//		printf("hehe\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 3;
+//	if (5 == num)
+//		printf("hehe\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (0==num % 2)
+//		printf("%d为偶数\n",num);
+//	else
+//		printf("%d为奇数\n",num);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		if (1 == i % 2)
+//		{
+//			printf("%d\n", i);
+//		}
+//		i++;
+//	}
+//}
+//简单方法
+//#include<stdio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 100; i += 2)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break; 
+//	case 4:
+//		printf("星期四\n");
+//			break; 
+//	case 5:
+//		printf("星期五\n");
+//				break; 
+//	case 6:
+//		printf("星期六\n");
+//					break;
+//	case 7:
+//		printf("星期日\n");
+//						break;
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+		printf("星期一\n");
+		break;
+	case 2:
+		printf("星期二\n");
+		break;
+	case 3:
+		printf("星期三\n");
+		break;
+	case 4:
+		printf("星期四\n");
+		break;
+	case 5:
+		printf("星期五\n");
+		break;
+	case 6:
+		printf("星期六\n");
+		break;
+	case 7:
+		printf("星期日\n");
+		break;
+	default:
+		printf("输入错误\n");
+	}
+	return 0;
+}
