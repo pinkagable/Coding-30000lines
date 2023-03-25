5d8e0b940b645ff3d656696edf7db75ea6156b2b
pinkagable <112944135+pinkagable@users.noreply.github.com>
2023-03-25 22:06:39 +0800

    Add files via upload

--- Coding_30000lines/Coding_30000lines.c
+++ Coding_30000lines/Coding_30000lines.c
@@ -756,26 +756,219 @@ int main()
 //	goto flag;
 //	return 0;
 //}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 300");
+//again:
+//	printf("请注意，你的电脑将在1分钟内关机，输入：我是猪，取消关机\n");
+//	scanf("%s", input);
+//	if (strcmp(input, "我是猪") == 0)//两个字符串比较不能用等号
+//	{
+//		printf("不关机了\n");
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[20] = "hello world";
+//	strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//	return 0;
+//}
+//#include<stdio.h>
+//get_max(int x, int y)
+//{
+//	int z = 0;
+//	if (x > y)
+//		z = x;
+//	else
+//		z = y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//函数的调用
+//	int max = get_max(a, b);
+//	printf("max=%d\n", max);
+//		return 0;
+//}
+//#include<stdio.h>
+//void Swap(int x, int y)
+//{
+//	int t=0;
+//	t = x;
+//	x = y;
+//	y = t;
+//}
+//Swap被调用的时候，实参传递给形参，其实形参是实参的一份临时拷贝。改变形参不能改变实参
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("交换前：a=%d,b=%d\n",a,b);
+//	Swap(a,b);
+//	printf("交换后:a=%d,b=%d", a, b);
+//	return 0;
+//}//无法交换x，y，只是交换了函数里的新的xy
+//#include<stdio.h>
+//void swap(int* pa, int* pb)
+//{
+//	int z = 0;
+//	z = *pa;
+//	*pa = *pb;
+//	*pb = z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int* pa = &a;//pa就是一个指针变量
+//	printf("交换前:a = % d, b = % d\n", a, b);
+//	swap(&a, &b);
+//	printf("交换后:a = % d, b = % d \n", a, b);
+//}
+//写一个函数判断一个数是不是素数
+//#include<stdio.h>
+//#include<math.h>
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for(j=2;j<n;j++)
+//	{
+//		if (n % j == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//
+//		if (is_prime(i) == 1)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}	
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+//#include<stdio.h>
+//int is_leap_year(int n)
+//{
+//	int j = 0;
+//	if (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0))
+//	{
+//		return 1;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int count = 0;
+//	int y = 0;
+//	for (y = 1000; y <= 2000; y++)
+//	{
+//		if (is_leap_year(y) == 1)
+//		{
+//			count++;
+//			printf("%d ", y);
+//		}
+//
+//	}
+//	printf("%d\n", count);
+//		return 0;
+//}
+//写一个函数，实现一个整型有序数组的二分查找
+//#include<stdio.h>
+//#include<string.h>
+//int binary_search(int a[], int k, int s)
+//{
+//	int left = 0;
+//	int right = s - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (a[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (a[mid] < k )
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;//找不到了
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int key = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//找到了就返回找到的位置的下表，找不到返回一个-1
+//	int ret=binary_search(arr,key,sz);
+//	if (-1==ret)
+//	{
+//		printf("找不到\n");
+//	}
+//	else 
+//	{
+//		printf("找到了，下标是%d\n", ret);
+//	}
+//	return 0;
+//}
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+void Add(int* p)
+{
+	(*p)++;
+}
 int main()
 {
-	char input[20] = { 0 };
-	system("shutdown -s -t 300");
-again:
-	printf("请注意，你的电脑将在1分钟内关机，输入：我是猪，取消关机\n");
-	scanf("%s", input);
-	if (strcmp(input, "我是猪") == 0)//两个字符串比较不能用等号
-	{
-		printf("不关机了\n");
-		system("shutdown -a");
-	}
-	else
-	{
-		goto again;
-	}
+	int num = 0;
+	Add(&num);
+	printf("%d\n", num);
+	Add(&num);
+	printf("%d\n", num);
+	Add(&num);
+	printf("%d\n", num);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
