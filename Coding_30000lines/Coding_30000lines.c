05ef7c4e79d653961b2b373e450ec6afc0afdc30
pinkagable <112944135+pinkagable@users.noreply.github.com>
2023-04-02 18:28:31 +0800

    Add files via upload

--- Coding_30000lines/Coding_30000lines.c
+++ Coding_30000lines/Coding_30000lines.c
@@ -1041,27 +1041,28 @@ int main()
 //	printf("%d", Fib(n));
 //	return 0;
 //}
-#include<stdio.h>
-int Fib(int n)
-{
-	int a = 1;
-	int b = 1;
-	int c = 1;
-	while (n>=2)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	return c;
-}
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	printf("%d", Fib(n));
-}
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n>=2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", Fib(n));
+//	return 0;
+//}
 
 
 
